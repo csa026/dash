101bd792ebad1ae98f8e4a9888c7668cfb63935f
csa026 <66832390+csa026@users.noreply.github.com>
2024-08-09 17:16:51 +0800

    Update header.tsx

--- app/(main)/header.tsx
+++ app/(main)/header.tsx
@@ -10,26 +10,6 @@ function Header() {
   return (
     <div className="mx-auto w-full max-w-5xl">
       <section className="flex items-center justify-between">
-        <section className="text-md flex items-center font-medium">
-          <div className="mr-1 flex flex-row items-center justify-start">
-            <Image
-              width={40}
-              height={40}
-              unoptimized
-              alt="apple-touch-icon"
-              src={"/apple-touch-icon.png"}
-              className="relative !m-0 h-6 w-6 border-2 border-white object-cover object-top !p-0 transition duration-500 group-hover:z-30 group-hover:scale-105"
-            />
-          </div>
-          NezhaDash
-          <Separator
-            orientation="vertical"
-            className="mx-2 hidden h-4 w-[1px] md:block"
-          />
-          <p className="hidden text-sm font-medium opacity-40 md:block">
-            Simple and beautiful dashboard
-          </p>
-        </section>
         <ModeToggle />
       </section>
       <Overview />
@@ -72,9 +52,9 @@ function Overview() {
 
   return (
     <section className={"mt-10 flex flex-col md:mt-16"}>
-      <p className="text-md font-semibold">👋 Overview</p>
+      <p className="text-md font-semibold">👋 HH_VPS Status</p>
       <div className="flex items-center gap-1.5">
-        <p className="text-sm font-medium opacity-50">where the time is</p>
+        <p className="text-sm font-medium opacity-50">Current Time: </p>
         {mouted && (
           <p className="opacity-1 text-sm font-medium">{timeString}</p>
         )}
