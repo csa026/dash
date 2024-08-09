e32fde5371dd425a390bcd1c4dbf63d96795ad80
csa026 <66832390+csa026@users.noreply.github.com>
2024-08-09 17:47:14 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -1,6 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mx-auto w-full max-w-5xl">
+      <section className="flex flex-col">
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           © 1976-{new Date().getFullYear()}{" "}
           <a href={"https://vps.henryho.top"}>@HH_VPS</a>
